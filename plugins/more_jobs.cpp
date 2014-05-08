c3457bd11d532e4b47f7d246f46837cb68cf30fd
Quietust <quietust@gmail.com>
2014-05-08 08:58:47 -0500

    Extend more_jobs to cover the job manager queue as well

--- plugins/more_jobs.cpp
+++ plugins/more_jobs.cpp
@@ -1,16 +1,18 @@
-// Add some additional jobs at certain appropriate workshops
-// Carpenter's Workshop - wooden blocks, wooden trap components
-// Metalsmith's/Magma Forge - metal mechanisms
-// [Magma] Glass Furnace - glass trap components
-// Also extends Blocks stockpiles to allow selection of Wood
-
-// Currently does not add these to the Job Manager search list
+// Updates several workshops to make some additional jobs available (as in later versions of DF)
+// * Carpenter's Workshop - wooden blocks, wooden trap components
+// * Metalsmith's/Magma Forge - metal mechanisms
+// * (Magma) Glass Furnace - glass trap components
+// Extends Blocks stockpiles to allow selection of Wood
+// Extends Job Manager to allow adding jobs for wooden blocks and wood/glass trap components
+// (mechanism jobs are not added since they would go to the wrong workshops)
+// Removes Encrust jobs from Job Manager, since Encrust work orders are broken in this version of DF
 
 #include "Core.h"
 #include <Console.h>
 #include <Export.h>
 #include <PluginManager.h>
 #include <MiscUtils.h>
+#include <modules/Screen.h>
 #include <vector>
 #include <string>
 #include <set>
@@ -28,6 +30,9 @@
 #include "df/historical_entity.h"
 #include "df/viewscreen_layer_stockpilest.h"
 #include "df/layer_object_listst.h"
+#include "df/viewscreen_jobmanagementst.h"
+#include "df/viewscreen_createquotast.h"
+#include "df/manager_order_template.h"
 
 using std::set;
 using std::vector;
@@ -196,9 +201,195 @@ struct stockpile_hook : df::viewscreen_layer_stockpilest
     }
 };
 
+bool init_createquota = false;
+struct jobmanagement_hook : df::viewscreen_jobmanagementst
+{
+    typedef df::viewscreen_jobmanagementst interpose_base;
+
+    DEFINE_VMETHOD_INTERPOSE(void, view, ())
+    {
+        init_createquota = Screen::isKeyPressed(interface_key::MANAGER_NEW_ORDER);
+        INTERPOSE_NEXT(view)();
+    }
+};
+
+struct createquota_hook : df::viewscreen_createquotast
+{
+    typedef df::viewscreen_createquotast interpose_base;
+
+    DEFINE_VMETHOD_INTERPOSE(void, view, ())
+    {
+        df::manager_order_template *order;
+        int offset;
+
+        if (init_createquota)
+        {
+            init_createquota = false;
+
+            df::historical_entity *entity = df::historical_entity::find(ui->civ_id);
+            if (!entity)
+            {
+                // if this happens, something went seriously wrong...
+                INTERPOSE_NEXT(view)();
+                return;
+            }
+
+            // Step 1: Delete all "Encrust" orders, since they don't actually work in this version
+            // (manager_order doesn't have the "item_category" field)
+            for (size_t i = 0; i < orders.size(); i++)
+            {
+                if ((orders[i]->job_type == job_type::EncrustWithGems) || (orders[i]->job_type == job_type::EncrustWithGlass))
+                {
+                    delete orders[i];
+                    orders.erase(orders.begin() + i);
+                    i--;
+                }
+            }
+
+            // Step 2: Add new jobs where appropriate
+
+            // Part 1 - wooden blocks
+            offset = orders.size();
+            for (size_t i = 0; i < orders.size(); i++)
+                if ((orders[i]->job_type == job_type::MakeToy) && (orders[i]->material == material_type::WOOD))
+                {
+                    offset = i;
+                    break;
+                }
+
+            order = df::allocate<df::manager_order_template>();
+
+            order->job_type = job_type::ConstructBlocks;
+            order->item_subtype = -1;
+            order->material = material_type::WOOD;
+            order->visible = true;
+            order->item_category.whole = 0;
+
+            orders.insert(orders.begin() + ++offset, order);
+
+            // Part 2 - wooden trap components
+            offset = orders.size();
+            for (size_t i = 0; i < orders.size(); i++)
+                if ((orders[i]->job_type == job_type::MakeCage) && (orders[i]->material == material_type::WOOD))
+                {
+                    offset = i;
+                    break;
+                }
+            for (size_t i = 0; i < entity->resources.trapcomp_type.size(); i++)
+            {
+                df::itemdef_trapcompst *itemdef = df::itemdef_trapcompst::find(entity->resources.trapcomp_type[i]);
+                if (!itemdef)
+                    continue;
+
+                // 23a didn't have material flags for trap components, so we need to filter by damage type
+                if ((itemdef->damage_type != damage_type::BLUDGEON) && (itemdef->damage_type != damage_type::PIERCE))
+                    continue;
+
+                order = df::allocate<df::manager_order_template>();
+
+                order->job_type = job_type::MakeTrapComponent;
+                order->item_subtype = itemdef->subtype;
+                order->material = material_type::WOOD;
+                order->visible = true;
+                order->item_category.whole = 0;
+
+                orders.insert(orders.begin() + ++offset, order);
+            }
+
+            // Part 3 - green glass trap components
+            offset = orders.size();
+            for (size_t i = 0; i < orders.size(); i++)
+                if ((orders[i]->job_type == job_type::MakeCage) && (orders[i]->material == material_type::GLASS_GREEN))
+                {
+                    offset = i;
+                    break;
+                }
+
+            for (size_t i = 0; i < entity->resources.trapcomp_type.size(); i++)
+            {
+                df::itemdef_trapcompst *itemdef = df::itemdef_trapcompst::find(entity->resources.trapcomp_type[i]);
+                if (!itemdef)
+                    continue;
+
+                order = df::allocate<df::manager_order_template>();
+
+                order->job_type = job_type::MakeTrapComponent;
+                order->item_subtype = itemdef->subtype;
+                order->material = material_type::GLASS_GREEN;
+                order->visible = true;
+                order->item_category.whole = 0;
+
+                orders.insert(orders.begin() + ++offset, order);
+            }
+
+            // Part 4 - clear glass trap components
+            offset = orders.size();
+            for (size_t i = 0; i < orders.size(); i++)
+                if ((orders[i]->job_type == job_type::MakeCage) && (orders[i]->material == material_type::GLASS_CLEAR))
+                {
+                    offset = i;
+                    break;
+                }
+
+            for (size_t i = 0; i < entity->resources.trapcomp_type.size(); i++)
+            {
+                df::itemdef_trapcompst *itemdef = df::itemdef_trapcompst::find(entity->resources.trapcomp_type[i]);
+                if (!itemdef)
+                    continue;
+
+                order = df::allocate<df::manager_order_template>();
+
+                order->job_type = job_type::MakeTrapComponent;
+                order->item_subtype = itemdef->subtype;
+                order->material = material_type::GLASS_CLEAR;
+                order->visible = true;
+                order->item_category.whole = 0;
+
+                orders.insert(orders.begin() + ++offset, order);
+            }
+
+            // Part 5 - crystal glass trap components
+            offset = orders.size();
+            for (size_t i = 0; i < orders.size(); i++)
+                if ((orders[i]->job_type == job_type::MakeCage) && (orders[i]->material == material_type::GLASS_CRYSTAL))
+                {
+                    offset = i;
+                    break;
+                }
+
+            for (size_t i = 0; i < entity->resources.trapcomp_type.size(); i++)
+            {
+                df::itemdef_trapcompst *itemdef = df::itemdef_trapcompst::find(entity->resources.trapcomp_type[i]);
+                if (!itemdef)
+                    continue;
+
+                order = df::allocate<df::manager_order_template>();
+
+                order->job_type = job_type::MakeTrapComponent;
+                order->item_subtype = itemdef->subtype;
+                order->material = material_type::GLASS_CRYSTAL;
+                order->visible = true;
+                order->item_category.whole = 0;
+
+                orders.insert(orders.begin() + ++offset, order);
+            }
+
+            // Metal mechanisms are NOT included here, since the game doesn't add them to the proper workshop
+
+            // Step 3: Rebuild page_indices
+            page_indices.clear();
+            for (int i = 0; i < orders.size(); i += 32)
+                page_indices.push_back(i);
+        }
+        INTERPOSE_NEXT(view)();
+    }
+};
+
 IMPLEMENT_VMETHOD_INTERPOSE(workshop_hook, populateTaskList);
 IMPLEMENT_VMETHOD_INTERPOSE(furnace_hook, populateTaskList);
 IMPLEMENT_VMETHOD_INTERPOSE(stockpile_hook, view);
+IMPLEMENT_VMETHOD_INTERPOSE(jobmanagement_hook, view);
+IMPLEMENT_VMETHOD_INTERPOSE(createquota_hook, view);
 
 DFHACK_PLUGIN("more_jobs");
 DFHACK_PLUGIN_IS_ENABLED(is_enabled);
@@ -212,6 +403,10 @@ DFhackCExport command_result plugin_enable(color_ostream &out, bool enable)
         if (!INTERPOSE_HOOK(furnace_hook, populateTaskList).apply(enable))
             return CR_FAILURE;
         if (!INTERPOSE_HOOK(stockpile_hook, view).apply(enable))
+            return CR_FAILURE;
+        if (!INTERPOSE_HOOK(jobmanagement_hook, view).apply(enable))
+            return CR_FAILURE;
+        if (!INTERPOSE_HOOK(createquota_hook, view).apply(enable))
             return CR_FAILURE;
 
         is_enabled = enable;
@@ -230,5 +425,7 @@ DFhackCExport command_result plugin_shutdown ( color_ostream &out )
     INTERPOSE_HOOK(workshop_hook, populateTaskList).remove();
     INTERPOSE_HOOK(furnace_hook, populateTaskList).remove();
     INTERPOSE_HOOK(stockpile_hook, view).remove();
+    INTERPOSE_HOOK(jobmanagement_hook, view).remove();
+    INTERPOSE_HOOK(createquota_hook, view).remove();
     return CR_OK;
 }
