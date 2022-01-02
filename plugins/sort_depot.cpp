6096c385cc70db363d8d680745d7da4a973b05d5
Quietust <quietust@gmail.com>
2022-01-01 18:22:39 -0600

    Update sort_depot, add a Search option

--- plugins/sort_depot.cpp
+++ plugins/sort_depot.cpp
@@ -1,4 +1,5 @@
 // Improve "Bring to Depot" screen - show item value, and allow sorting by value or distance
+// Also add a Search field for finding desired trade goods
 
 #include "Core.h"
 #include <Console.h>
@@ -21,6 +22,7 @@
 #include "df/assign_trade_status.h"
 #include "df/building_tradedepotst.h"
 #include "df/ui.h"
+#include "df/interfacest.h"
 
 using std::set;
 using std::vector;
@@ -32,6 +34,7 @@ using namespace df::enums;
 using df::global::world;
 using df::global::ui;
 using df::global::gps;
+using df::global::gview;
 
 void OutputString(int8_t fg, int &x, int y, const std::string &text)
 {
@@ -77,6 +80,11 @@ bool trade_pair_sort (const trade_pair &t1, const trade_pair &t2)
 
 bool inHook = false;
 df::viewscreen_assigntradest *inHook_viewscreen = NULL;
+
+stl::vector<df::assign_trade_status *> all_trades;
+std::string filter_string;
+bool in_filter;
+
 struct assigntrade_hook : df::viewscreen_assigntradest
 {
     typedef df::viewscreen_assigntradest interpose_base;
@@ -85,6 +93,68 @@ struct assigntrade_hook : df::viewscreen_assigntradest
     {
         bool have_broker = (ui->nobles_arrived[profession::BROKER] > ui->units_killed[profession::BROKER]);
 
+        if (!all_trades.size())
+            all_trades = info;
+        if (in_filter)
+        {
+            std::string filter = filter_string;
+            if (Screen::isKeyPressed(interface_key::STRING_BACKSPACE)) { if (!filter_string.empty()) filter_string.pop_back(); }
+            else if (Screen::isKeyPressed(interface_key::STRING_SPACE)) filter_string.push_back(' ');
+            else if (Screen::isKeyPressed(interface_key::STRING_A) || Screen::isKeyPressed(interface_key::STRING_A_CAP)) filter_string.push_back('A');
+            else if (Screen::isKeyPressed(interface_key::STRING_B) || Screen::isKeyPressed(interface_key::STRING_B_CAP)) filter_string.push_back('B');
+            else if (Screen::isKeyPressed(interface_key::STRING_C) || Screen::isKeyPressed(interface_key::STRING_C_CAP)) filter_string.push_back('C');
+            else if (Screen::isKeyPressed(interface_key::STRING_D) || Screen::isKeyPressed(interface_key::STRING_D_CAP)) filter_string.push_back('D');
+            else if (Screen::isKeyPressed(interface_key::STRING_E) || Screen::isKeyPressed(interface_key::STRING_E_CAP)) filter_string.push_back('E');
+            else if (Screen::isKeyPressed(interface_key::STRING_F) || Screen::isKeyPressed(interface_key::STRING_F_CAP)) filter_string.push_back('F');
+            else if (Screen::isKeyPressed(interface_key::STRING_G) || Screen::isKeyPressed(interface_key::STRING_G_CAP)) filter_string.push_back('G');
+            else if (Screen::isKeyPressed(interface_key::STRING_H) || Screen::isKeyPressed(interface_key::STRING_H_CAP)) filter_string.push_back('H');
+            else if (Screen::isKeyPressed(interface_key::STRING_I) || Screen::isKeyPressed(interface_key::STRING_I_CAP)) filter_string.push_back('I');
+            else if (Screen::isKeyPressed(interface_key::STRING_J) || Screen::isKeyPressed(interface_key::STRING_J_CAP)) filter_string.push_back('J');
+            else if (Screen::isKeyPressed(interface_key::STRING_K) || Screen::isKeyPressed(interface_key::STRING_K_CAP)) filter_string.push_back('K');
+            else if (Screen::isKeyPressed(interface_key::STRING_L) || Screen::isKeyPressed(interface_key::STRING_L_CAP)) filter_string.push_back('L');
+            else if (Screen::isKeyPressed(interface_key::STRING_M) || Screen::isKeyPressed(interface_key::STRING_M_CAP)) filter_string.push_back('M');
+            else if (Screen::isKeyPressed(interface_key::STRING_N) || Screen::isKeyPressed(interface_key::STRING_N_CAP)) filter_string.push_back('N');
+            else if (Screen::isKeyPressed(interface_key::STRING_O) || Screen::isKeyPressed(interface_key::STRING_O_CAP)) filter_string.push_back('O');
+            else if (Screen::isKeyPressed(interface_key::STRING_P) || Screen::isKeyPressed(interface_key::STRING_P_CAP)) filter_string.push_back('P');
+            else if (Screen::isKeyPressed(interface_key::STRING_Q) || Screen::isKeyPressed(interface_key::STRING_Q_CAP)) filter_string.push_back('Q');
+            else if (Screen::isKeyPressed(interface_key::STRING_R) || Screen::isKeyPressed(interface_key::STRING_R_CAP)) filter_string.push_back('R');
+            else if (Screen::isKeyPressed(interface_key::STRING_S) || Screen::isKeyPressed(interface_key::STRING_S_CAP)) filter_string.push_back('S');
+            else if (Screen::isKeyPressed(interface_key::STRING_T) || Screen::isKeyPressed(interface_key::STRING_T_CAP)) filter_string.push_back('T');
+            else if (Screen::isKeyPressed(interface_key::STRING_U) || Screen::isKeyPressed(interface_key::STRING_U_CAP)) filter_string.push_back('U');
+            else if (Screen::isKeyPressed(interface_key::STRING_V) || Screen::isKeyPressed(interface_key::STRING_V_CAP)) filter_string.push_back('V');
+            else if (Screen::isKeyPressed(interface_key::STRING_W) || Screen::isKeyPressed(interface_key::STRING_W_CAP)) filter_string.push_back('W');
+            else if (Screen::isKeyPressed(interface_key::STRING_X) || Screen::isKeyPressed(interface_key::STRING_X_CAP)) filter_string.push_back('X');
+            else if (Screen::isKeyPressed(interface_key::STRING_Y) || Screen::isKeyPressed(interface_key::STRING_Y_CAP)) filter_string.push_back('Y');
+            else if (Screen::isKeyPressed(interface_key::STRING_Z) || Screen::isKeyPressed(interface_key::STRING_Z_CAP)) filter_string.push_back('Z');
+            else if (Screen::isKeyPressed(interface_key::SELECT)) in_filter = false;
+            if (filter_string.length() > 30) { filter_string[29] = filter_string[30]; filter_string.pop_back(); }
+            gview->current_key = 0;
+            if (filter != filter_string)
+            {
+                filter = filter_string;
+                std::transform(filter.begin(), filter.end(), filter.begin(), ::tolower);
+                info.clear();
+                for (int i = 0; i < all_trades.size(); i++)
+                {
+                    stl::string _name;
+                    all_trades[i]->item->getItemDescription(&_name, 0);
+                    std::string name(_name);
+                    std::transform(name.begin(), name.end(), name.begin(), ::tolower);
+                    if (name.find(filter) != std::string::npos)
+                        info.push_back(all_trades[i]);
+                }
+                if (cursor > info.size())
+                    cursor = info.size() - 1;
+            }
+        }
+        if (Screen::isKeyPressed(interface_key::STRING_S))
+            in_filter = true;
+        if (Screen::isKeyPressed(interface_key::LEAVESCREEN))
+        {
+            info = all_trades;
+            all_trades.clear();
+            filter_string.clear();
+        }
         if (Screen::isKeyPressed(interface_key::STRING_V_CAP) && have_broker)
         {
             vector<trade_pair> values;
@@ -176,6 +246,10 @@ DFhackCExport command_result plugin_onrender ( color_ostream &out)
 
             OutputString(14, x, i + 2, have_broker ? stl_sprintf("%10i\x0F", val) : "           ");
         }
+
+        x = 34;
+        OutputString(12, x, 22, Screen::getKeyDisplay(interface_key::STRING_S));
+        OutputString(in_filter ? 15 : 7, x, 22, ": Select " + filter_string + "_");
 
         x = 2;
         if (have_broker)
